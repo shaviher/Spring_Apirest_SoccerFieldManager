b5da3e56bea4c1b24cba3cda860d89522bae77d4
Javier <javiherdeza@gmail.com>
2024-09-02 12:17:19 -0600

    Models, utils y db

--- src/main/java/com/javierhernandez/soccerfieldmanager/models/Reservation.java
+++ src/main/java/com/javierhernandez/soccerfieldmanager/models/Reservation.java
@@ -1,5 +1,38 @@
 package com.javierhernandez.soccerfieldmanager.models;
 
-public class Reservation {
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.io.ObjectInputFilter.Status;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name ="reservation")
+
+public class Reservation implements Serializable{
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date startTime;
+    private Date endTime;
+    private String payment;
+
+    private Status status;
+
+    @ManyToOne // De esta clase a la otra (many la clase osea reservation y to one hacia la otra clase en este caso User obj user)
+    private User user;
+
+    @ManyToOne
+    private SoccerField soccerField;
 
 }
---
+++ src/main/java/com/javierhernandez/soccerfieldmanager/utils/Status.java
@@ -0,0 +1,7 @@
+package com.javierhernandez.soccerfieldmanager.utils;
+
+public enum Status {
+    PROCESS,
+    RESERVED,
+    CANCELLED;
+}
